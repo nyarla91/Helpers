8b9674896e28b6f6f7bb601102ed47bd5b7d6110
nyarla <nyarladev@gmail.com>
2021-08-21 11:01:37 +0300

    Main

---
+++ Bezier.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Vector3 = UnityEngine.Vector3;
+
+public static class Bezier
+{
+    public static Vector3 Evaluate(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        // 1st iteration
+        Vector3 p01 = Vector3.Lerp(p0, p1, t);
+        Vector3 p12 = Vector3.Lerp(p1, p2, t);
+        Vector3 p23 = Vector3.Lerp(p2, p3, t);
+        // 2nditeration
+        Vector3 p012 = Vector3.Lerp(p01, p12, t);
+        Vector3 p123 = Vector3.Lerp(p12, p23, t);
+        // Result
+        return Vector3.Lerp(p012, p123, t);
+    }
+    
+    public static Vector3 Evaluate(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        // 1st iteration
+        Vector3 p01 = Vector3.Lerp(p0, p1, t);
+        Vector3 p12 = Vector3.Lerp(p1, p2, t);
+        // Result
+        return Vector3.Lerp(p01, p12, t);
+    }
+    
+    public static Vector3 Evaluate(Vector3[] points, float t)
+    {
+        for (int i = points.Length - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                points[j] = Vector3.Lerp(points[j], points[j + 1], t);
+            }
+        }
+        return points[0];
+    }
+
+    public static Vector3[] EvaluatePath(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, int quality)
+    {
+        quality--;
+        if (quality < 1)
+            quality = 1;
+        Vector3[] result = new Vector3[quality + 1];
+        for (int i = 0; i < result.Length; i++)
+        {
+            float t = (float) i / (float) quality;
+            result[i] = Evaluate(p0, p1, p2, p3, t);
+        }
+        return result;
+    }
+
+    public static Vector3[] EvaluatePath(Vector3 p0, Vector3 p1, Vector3 p2, int quality)
+    {
+        quality--;
+        if (quality < 1)
+            quality = 1;
+        Vector3[] result = new Vector3[quality + 1];
+        for (int i = 0; i < result.Length; i++)
+        {
+            float t = (float) i / (float) quality;
+            result[i] = Evaluate(p0, p1, p2, t);
+        }
+        return result;
+    }
+}
---
+++ CameraProperties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraProperties : Transformer
+{
+    public static CameraProperties instance;
+    private static Camera _main;
+    public static Camera Main => _main ?? (_main = instance.GetComponent<Camera>());
+    
+    public static Vector3 MousePosition2D => Main.ScreenToWorldPoint(Input.mousePosition);
+
+    public static Vector3 PercentToScreenPoint(Vector2 percent)
+    {
+        percent += Vector2.one;
+        percent /= 2;
+        return new Vector2(Screen.width, Screen.height);
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
